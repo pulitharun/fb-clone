2785ed68b75a47546101a180c8fa1c32ab5687a4
Puli Tharun <pulitharun21@gmail.com>
2024-04-28 02:58:22 +0530

    Facebook-Clone

---
+++ src/Header.js
@@ -0,0 +1,61 @@
+import React from "react";
+import "./Header.css";
+import SearchIcon from "@mui/icons-material/Search"
+import HomeIcon from "@mui/icons-material/Home";
+import FlagIcon from "@mui/icons-material/Flag";
+import SubscriptionsIcon from "@mui/icons-material/Subscriptions";
+import StorefrontIcon from "@mui/icons-material/Storefront";
+import SupervisedUserCircleIcon from "@mui/icons-material/SupervisedUserCircle";
+import { Avatar, IconButton } from "@mui/material";
+import AddIcon from '@mui/icons-material/Add';
+import ForumIcon from '@mui/icons-material/Forum';
+import NotificationsActiveIcon from '@mui/icons-material/NotificationsActive';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="header__left"></div>
+      <img
+        alt="Facebook logo"
+        // src="//upload.wikimedia.org/wikipedia/commons/thumb/9/93/Facebook_logo_%282023%29.svg/150px-Facebook_logo_%282023%29.svg.png"
+        src="//upload.wikimedia.org/wikipedia/commons/thumb/b/b9/2023_Facebook_icon.svg/50px-2023_Facebook_icon.svg.png" />
+
+      <div className="header__input">
+        <SearchIcon />
+        <input placeholder="Search Facebook " type="text" />
+      </div>
+
+      <div className="header__center">
+        <div className="header__option header__option--active">
+          <HomeIcon fontSize="large" />
+        </div>
+        <div className="header__option">
+          <FlagIcon fontSize="large" />
+        </div>
+        <div className="header__option">
+          <SubscriptionsIcon fontSize="large" />
+        </div>
+        <div className="header__option">
+          <StorefrontIcon fontSize="large" />
+        </div>
+        <div className="header__option">
+          <SupervisedUserCircleIcon fontSize="large" />
+        </div>
+      </div>
+
+      <div className="header__right">
+        <div className="header__info">
+            <Avatar/>
+            <h4>lufffffyyyyy</h4>
+        </div>
+        <IconButton> <AddIcon/> </IconButton>
+        <IconButton> <ForumIcon/> </IconButton>
+        <IconButton> <NotificationsActiveIcon/> </IconButton>
+        <IconButton> <ExpandMoreIcon/> </IconButton>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyA2UIF60VHf5FifHNy0FJI4ZbAcNppPX0A",
+    authDomain: "facebook-clone-f1683.firebaseapp.com",
+    projectId: "facebook-clone-f1683",
+    storageBucket: "facebook-clone-f1683.appspot.com",
+    messagingSenderId: "821576871118",
+    appId: "1:821576871118:web:ef8bdbff9b9cb710ee749f",
+    measurementId: "G-66FHBRCFPQ"
+  };
+
+
+  
